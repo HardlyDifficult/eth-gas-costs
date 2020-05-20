5221c79139638344e9b94e1525494ff1a8edada3
NickCuso <hardlydiff@gmail.com>
2020-05-20 13:03:23 -0700

    Lint

--- test/types/Type_bool.js
+++ test/types/Type_bool.js
@@ -1,45 +1,45 @@
 const ContractArtifact = artifacts.require("Type_bool");
 
-contract("Type_bool", accounts =>{
-    let contract;
+contract("Type_bool", (accounts) => {
+  let contract;
 
-    beforeEach(async () =>{
-        contract = await ContractArtifact.new();
+  beforeEach(async () => {
+    contract = await ContractArtifact.new();
+  });
+
+  it("acceptParam(true)", async () => {
+    await contract.acceptParam(true);
+  });
+
+  it("acceptParam(false)", async () => {
+    await contract.acceptParam(false);
+  });
+
+  describe("with value false", () => {
+    beforeEach(async () => {
+      await contract.setFalse();
     });
 
-    it("acceptParam(true)", async () =>{
-        await contract.acceptParam(true);
+    it("Set False", async () => {
+      await contract.setFalse();
     });
 
-    it("acceptParam(false)", async () =>{
-        await contract.acceptParam(false);
+    it("Set True", async () => {
+      await contract.setTrue();
+    });
+  });
+
+  describe("with value true", () => {
+    beforeEach(async () => {
+      await contract.setTrue();
     });
 
-    describe("with value false", () =>{
-        beforeEach(async () =>{
-            await contract.setFalse();
-        });
-
-        it("Set False", async () =>{
-            await contract.setFalse();
-        });
-
-        it("Set True", async () =>{
-            await contract.setTrue();
-        });
+    it("Set False", async () => {
+      await contract.setFalse();
     });
-    
-    describe("with value true", () =>{
-        beforeEach(async () =>{
-            await contract.setTrue();
-        });
 
-        it("Set False", async () =>{
-            await contract.setFalse();
-        });
-
-        it("Set True", async () =>{
-            await contract.setTrue();
-        });
+    it("Set True", async () => {
+      await contract.setTrue();
     });
+  });
 });
